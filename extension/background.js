0205e6c9f8c1502cc55ff4a2a788980c684af71e
Avi Itskovich <avi.itskovich@airbnb.com>
2013-10-27 18:49:29 -0700

    Add Go link suggestions

--- extension/background.js
+++ extension/background.js
@@ -7,3 +7,35 @@ function navigate(url) {
 chrome.omnibox.onInputEntered.addListener(function(text) {
   navigate('http://gomarks.herokuapp.com/' + text);
 });
+
+function search(query, callback) {
+  var url = "http://gomarks.herokuapp.com/links/suggest?q=" + query;
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
+      var data = JSON.parse(req.responseText);
+      callback(data);
+    }
+  }
+  req.send(null);
+  return req;
+}
+
+chrome.omnibox.onInputChanged.addListener(
+    function(text, suggest) {
+      search(text, function(data) {
+        var suggestions = [];
+        for (var item in data) {
+          suggestions.push({
+            content: data[item],
+            description: data[item],
+          });
+        }
+        suggest(suggestions);
+
+        chrome.omnibox.setDefaultSuggestion({
+          description: text
+        });
+      });
+    });
